b27a551f2669732727519379eaa8c576e3a74557
Werner Altewischer <werner.altewischer@ing.com>
2019-04-01 12:50:34 +0200

    Updated version

--- Source/CarthageKit/CarthageKitVersion.swift
+++ Source/CarthageKit/CarthageKitVersion.swift
@@ -4,5 +4,5 @@ import SPMUtility
 public struct CarthageKitVersion {
     public let value: Version
 
-    public static let current = CarthageKitVersion(value: Version(0, 32, 0, buildMetadataIdentifiers: ["nsoperations"]))
+    public static let current = CarthageKitVersion(value: Version(0, 33, 0, buildMetadataIdentifiers: ["nsoperations"]))
 }
