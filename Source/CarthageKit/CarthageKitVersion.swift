30c75121fc457a3e2cafa060bbf5adb3655035e2
Werner Altewischer <werner.altewischer@ing.com>
2019-07-01 14:10:54 +0200

    Updated carthage version

--- Source/CarthageKit/CarthageKitVersion.swift
+++ Source/CarthageKit/CarthageKitVersion.swift
@@ -1,6 +1,5 @@
 /// Defines the current CarthageKit version.
 public struct CarthageKitVersion {
     public let value: SemanticVersion
-
-    public static let current = CarthageKitVersion(value: SemanticVersion(0, 36, 1, prereleaseIdentifiers: [], buildMetadataIdentifiers: ["nsoperations"]))
+    public static let current = CarthageKitVersion(value: SemanticVersion(0, 36, 2, prereleaseIdentifiers: [], buildMetadataIdentifiers: ["nsoperations"]))
 }
